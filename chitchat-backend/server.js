f551c9be3036cb775033bdec4170312f663a5bf1
raza <razakhanha05@gmail.com>
2023-10-08 12:12:39 +0530

    Intialized chitchat backend

---
+++ chitchat-backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { chats } = require("./data/data.js");
+const dotenv = require("dotenv");
+
+dotenv.config(); // Load environment variables from .env file
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello world ");
+});
+
+//getting all the chats data
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+//single chat data
+app.get("/api/chat/:id", (req, res) => {
+  console.log(req.params.id);
+  const singleChatData = chats.find((c) => c._id === req.params.id);
+  res.send(singleChatData);
+});
+
+const PORT = process.env.PORT || 5400;
+app.listen(console.log(`Server is running on port ${PORT} `));
