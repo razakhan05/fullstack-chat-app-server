cf1dfa4b0b980452f155c7886727916d05e31b66
raza <razakhanha05@gmail.com>
2023-10-17 12:41:47 +0530

    add front-end url

--- chitchat-backend/server.js
+++ chitchat-backend/server.js
@@ -42,7 +42,7 @@ server.listen(PORT, () => {
 const io = new Server(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
   },
 });
 
